1a335d41d60b40efd9ef81d5ce45ff9ff62e354d
TOOBA_ABDUL_RAZZAQ <haya84885@gmail.com>
2023-04-07 11:06:26 -0700

    second commit

--- script/app.js
+++ script/app.js
@@ -38,7 +38,102 @@
 //  var newNum = originalNum + numToBeAdded;
 // alert(newNum)
 
-var originalNum = 90;
- originalNum = originalNum + 10;
- alert(originalNum)
+// var originalNum = 90;
+//  originalNum = originalNum + 10;
+//  alert(originalNum)
 
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+//  alert(newNum)
+
+// var 1stPresident = "Washington";
+// alert(1stPresident)
+
+// alert(144);
+
+// var caseQty = 144;
+// alert(caseQty);
+
+//  variable types
+// userResponse
+// userResponseTime
+// userResponseTimeLimit
+// response
+
+// var popularNumber = 4;
+// alert(popularNumber)
+
+// var popularNumber = 2 + 2;
+// alert(popularNumber)
+
+// alert(2 + 2);
+
+// var popularNumber = 12 - 24;
+// alert(popularNumber)
+
+// var popularNumber = 3 * 12;
+// alert(popularNumber)
+
+// var num = 10;
+//  var popularNumber = num + 200;
+//  alert(popularNumber)
+//  var num = 10;
+//  var anotherNum = 1;
+//  var popularNumber = num + anotherNum;
+//  alert(popularNumber)
+
+// var whatsLeftOver = 10 % 3;
+// alert(whatsLeftOver)
+//     var whatsLeftOver = 9 % 3;
+//     alert(whatsLeftOver)
+
+//  var num = 1;
+//  var newNum = num++;
+// alert(newNum)
+// var num = 1;
+//  var newNum = ++num;
+//  alert(newNum)
+
+//  var num = 1;
+//  var newNum = num--;
+// alert(newNum)
+// var num = 1;
+//  var newNum = --num;
+// alert(newNum)
+
+// var totalCost = 1 + 3 * 4;
+// alert(totalCost)
+// var totalCost = 1 + (3 * 4);
+// alert(totalCost)
+// var totalCost = (1 + 3) * 4;
+// alert(totalCost)
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// alert(resultOfComputation)
+// resultOfComputation = ((2 * 4) * 4) + 2;
+// alert(resultOfComputation)
+// resultOfComputation = (2 * 4) * (4 + 2);
+// alert(resultOfComputation)
+
+//  var message = "Thanks for your input!";
+//  alert(message);
+// alert("Thanks, " + userName + "!");
+// var message = "Thanks, ";
+//  var banger = "!";
+//  alert(message + userName + banger);
+//  alert("Thanks, " + "Susan" + "!");
+//  var message = "Thanks, ";
+//  var userName = "Susan";
+//  var banger = "!";
+//  var customMess = message + userName + banger;
+//  alert(customMess);
+// alert("2" + "2");
+// alert("2 plus 2 equals " + 2 + 2);
+
+var spec = prompt("Your species?", "human");
+var question = "Your species?";
+ var defaultAnswer = "human";
+ var spec = prompt(question, defaultAnswer);
+ alert(spec)
+ var numberOfCats = prompt("How many cats?");
+ var tooManyCats = numberOfCats + 1;
+ alert(tooManyCats)
