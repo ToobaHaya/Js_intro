da1ae608c33c432c7bc8ef8557bf5f074a6cc378
TOOBA_ABDUL_RAZZAQ <haya84885@gmail.com>
2023-04-07 10:25:19 -0700

    first commit

---
+++ script/app.js
@@ -0,0 +1,44 @@
+// 6-april-2023 day 1
+// alert("Thanks for your input!");
+
+// var uname = "Mark";
+// uname = "Ace";
+// alert(uname)
+
+// var lessonAuthor = "Mark";
+
+// var guyWhoKeepsSayingHisOwnName = "Mark";
+
+// var x = "Mark";
+// alert(lessonAuthor +" " + guyWhoKeepsSayingHisOwnName +" " + x)
+
+// var lastName = "Smith";
+// var cityOfOrigin = "New Orleans";
+// var aussieGreeting = "g'Day";
+
+// alert(lastName +" " +cityOfOrigin  +" " + aussieGreeting )
+
+// var nickname = "Bub";
+// alert(nickname)
+
+// alert("Thanks for your input!");
+
+// var thanx = "Thanks for your input!"
+// //  alert(thanx);
+
+
+ 
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+// alert(newNum)
+
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+//  var newNum = originalNum + numToBeAdded;
+// alert(newNum)
+
+var originalNum = 90;
+ originalNum = originalNum + 10;
+ alert(originalNum)
+
